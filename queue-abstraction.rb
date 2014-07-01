61e4e7523989a84e35dd823c2f39c84edc25986f
Steve Kemp <steve@steve.org.uk>
2014-07-01 21:11:51 +0100

    Added abstraction-layer.
      I'd foolishly forgotten to commit this.
    
      This is just a trivial layer which allows us to swap out the
     use of [redis](http://redis.io/) in the future.  I've added
     beanstalkd support because I know it and trust it, but it's
     not been field-tested yet.

---
+++ queue-abstraction.rb
@@ -0,0 +1,117 @@
+##
+## Trivial ClassFactory pattern to allow multiple queue-backends
+##
+
+
+
+#
+#  Attempt to load the libraries for our two backend modules.
+#
+#  This is setup because the user might only choose to use one backend
+# and not care if the other doesn't even load.
+#
+%w( beanstalk-client redis ).each do |library|
+  begin
+    require library
+  rescue LoadError
+    puts "Failed to load the library: #{library}"
+  end
+end
+
+
+
+
+
+#
+# This is a trivial abstraction layer between the Sinatra application
+# and the queue we use.
+#
+class Queue
+
+  #
+  # Class-Factory
+  #
+  def self.create type
+    case type
+    when "redis"
+      RedisQueue.new
+    when "beanstalk"
+      BeanstalkQueue.new
+    else
+      raise "Bad backend type: #{type}"
+    end
+  end
+
+
+  #
+  # Add the given JSON-encoded object to the queue.
+  #
+  def add( object )
+    raise "Subclasses must implement this method!"
+  end
+
+end
+
+
+
+
+
+#
+# The redis-based queue.
+#
+class RedisQueue < Queue
+
+
+  #
+  # Constructor.
+  #
+  def initialize
+    rehost = ENV["REDIS"] || "127.0.0.1"
+    @redis = Redis.new( :host => rehost )
+  end
+
+
+  #
+  # Add the JSON-encoded object to the queue.
+  #
+  def add( json )
+    @redis.rpush( "HOOK:JOBS", json )
+  end
+
+end
+
+
+
+
+
+
+
+
+
+#
+# The beanstalkd-based queue.
+#
+class BeanstalkQueue < Queue
+
+
+  #
+  # Constructor.
+  #
+  def initialize
+    @beanstalk = Beanstalk::Pool.new(['127.0.0.1:11300'])
+  end
+
+
+  #
+  # Add the JSON-encoded object to the queue.
+  #
+  def add( json )
+    @beanstalk.put( json )
+  end
+
+end
+
+
+
+
+
